64f80deedddf1eefc6ed127483de0b8aeef6c1b0
Natasha Araujo <Natasha Araujo@NATASHA>
2026-03-13 00:01:31 -0300

    Implementação da busca dinâmica com SearchBar no .NET MAUI

--- MauiAppMinhasCompras/Views/ListaProdutos.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProdutos.xaml.cs
@@ -1,10 +1,13 @@
 using MauiAppMinhasCompras.Helpers;
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
 
 namespace MauiAppMinhasCompras.Views
 {
     public partial class ListaProdutos : ContentPage
     {
         SQLiteDatabaseHelper _db;
+        ObservableCollection<Produto> listaProdutos = new ObservableCollection<Produto>();
 
         public ListaProdutos(SQLiteDatabaseHelper db)
         {
@@ -15,12 +18,26 @@ namespace MauiAppMinhasCompras.Views
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            cvProdutos.ItemsSource = await _db.GetAll();
+
+            var produtos = await _db.GetAll();
+            listaProdutos = new ObservableCollection<Produto>(produtos);
+            cvProdutos.ItemsSource = listaProdutos;
         }
 
         private async void OnNovoProdutoClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new NovoProduto(_db));
+        }
+
+        private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string texto = e.NewTextValue?.ToLower() ?? "";
+
+            var produtosFiltrados = listaProdutos
+                .Where(p => p.Descricao.ToLower().Contains(texto))
+                .ToList();
+
+            cvProdutos.ItemsSource = produtosFiltrados;
         }
     }
 }
